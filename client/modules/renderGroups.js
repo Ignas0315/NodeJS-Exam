3092e7abc4dfb5077eec7df0146f0ab27acc37fa
Ignas0315 <ignaszakaitis@gmail.com>
2023-06-30 22:59:30 +0300

    NODE-0009: Adding groups and parts of bills

--- client/modules/renderGroups.js
+++ client/modules/renderGroups.js
@@ -1,10 +1,47 @@
-import { getAllGroups } from './getGroups.js';
 import { getUserGroups } from './getGroups.js';
 
 const renderUserGroups = async () => {
-    const userGroups = await getUserGroups();
+    try {
+        const userGroups = await getUserGroups();
 
-    console.log(userGroups);
+        const groupsContainer = document.getElementById('groups-container');
+
+        console.log(userGroups);
+
+        if (userGroups === 0) {
+            const noGroupsHeading = document.createElement('h1');
+            noGroupsHeading.setAttribute('id', 'no-available-groups');
+            noGroupsHeading.innerHTML =
+                'You have no groups assigned. Please add some using form below.';
+            groupsContainer.appendChild(noGroupsHeading);
+        } else {
+            groupsContainer.innerHTML = '';
+            userGroups.forEach((group) => {
+                const groupId = group.id;
+                const groupName = group.name;
+
+                const groupCard = document.createElement('div');
+                const groupCardId = document.createElement('h2');
+                const groupCardName = document.createElement('p');
+                groupCard.setAttribute('class', 'group-card');
+                groupCardId.setAttribute('class', 'group-id');
+                groupCardName.setAttribute('class', 'group-name');
+
+                groupCard.addEventListener('click', () => {
+                    window.location.assign(`./bills.html?group_id=${groupId}`);
+                });
+
+                groupCardId.innerHTML = groupId;
+                groupCardName.innerHTML = groupName;
+
+                groupsContainer.appendChild(groupCard);
+                groupCard.appendChild(groupCardId);
+                groupCard.appendChild(groupCardName);
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export { renderUserGroups };
